97d8842560de604f41869fe995bef2d445b3660f
Nikita <nikoylb@gmail.com>
2017-05-13 00:34:07 +0400

    production_01

--- js/script.js
+++ js/script.js
@@ -2,6 +2,13 @@ var link = document.querySelector(".link");
 var modal = document.querySelector(".modal");
 var login = document.querySelector(".login");
 var close = document.querySelector(".close");
+var close_map = document.querySelector(".close_map");
+var close_write = document.querySelector(".close_write");
+var map_open = document.querySelector(".map_open");
+var map_modal = document.querySelector(".map_modal");
+var modal_write = document.querySelector(".modal_write");
+var write_me = document.querySelector(".write_me");
+
 
 link.addEventListener("click" , function(){
     modal.classList.toggle("show");
@@ -9,4 +16,20 @@ link.addEventListener("click" , function(){
 
 close.addEventListener("click", function(){
     modal.classList.remove("show");
+});
+
+map_open.addEventListener("click" , function(){
+	map_modal.classList.toggle("show");
+});
+
+close_map.addEventListener("click" , function(){
+	map_modal.classList.remove("show");
+});
+
+modal_write.addEventListener("click" , function(){
+    write_me.classList.toggle("show");
+});
+
+close_write.addEventListener("click" , function(){
+	write_me.classList.remove("show");
 });
